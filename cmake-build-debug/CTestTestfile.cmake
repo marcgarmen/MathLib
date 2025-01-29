25eea6e411605fe76c84861dc8586c13e7beeea6
Marco <marcoagarciam8@gmail.com>
2025-01-28 18:52:49 -0600

    pruebas y ultimos cambios

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/marca/OneDrive/Documents/GitHub/MathLib
+# Build directory: C:/Users/marca/OneDrive/Documents/GitHub/MathLib/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[AllTests]=] "C:/Users/marca/OneDrive/Documents/GitHub/MathLib/cmake-build-debug/test_suite.exe")
+set_tests_properties([=[AllTests]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/marca/OneDrive/Documents/GitHub/MathLib/CMakeLists.txt;39;add_test;C:/Users/marca/OneDrive/Documents/GitHub/MathLib/CMakeLists.txt;0;")
+subdirs("_deps/googletest-build")
