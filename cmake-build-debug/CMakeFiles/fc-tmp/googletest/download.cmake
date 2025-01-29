c0cf2c8d19d4bd593b297d53435a692869bf61b9
Marco <marcoagarciam8@gmail.com>
2025-01-28 19:15:35 -0600

    Ignorando archivos de cmake-build-debug

--- cmake-build-debug/CMakeFiles/fc-tmp/googletest/download.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION 3.29)
-
-message(VERBOSE "Executing download step for googletest")
-
-block(SCOPE_FOR VARIABLES)
-
-include("C:/Users/marca/OneDrive/Documents/GitHub/MathLib/cmake-build-debug/CMakeFiles/fc-tmp/googletest/googletest-gitclone.cmake")
-
-endblock()
--- cmake-build-debug/CMakeFiles/fc-tmp/googletest/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION 3.29)
-
-message(VERBOSE "Executing update step for googletest")
-
-block(SCOPE_FOR VARIABLES)
-
-include("C:/Users/marca/OneDrive/Documents/GitHub/MathLib/cmake-build-debug/CMakeFiles/fc-tmp/googletest/googletest-gitupdate.cmake")
-
-endblock()
